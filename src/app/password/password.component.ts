fa2f3335b541b61cafae0146ce6a5f247566fa68
olyak00 <olgakorniienko658@gmail.com>
2022-12-15 23:44:00 +0100

    first commit

---
+++ src/app/password/password.component.ts
@@ -0,0 +1,17 @@
+import {Component} from "@angular/core";
+import { NgModel, FormControl, FormGroup} from '@angular/forms';
+
+@Component({
+   selector:  'app-password',
+   templateUrl: './password.component.html',
+   styleUrls: ['./password.component.css']
+})
+export class PasswordComponent{
+  
+   password:string='';
+   
+
+}
+
+
+   
